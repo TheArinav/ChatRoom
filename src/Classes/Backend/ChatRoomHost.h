436bb9a075d990fe96b75fbe98e0433dbb9d7d4f
TheArinav <88740085+TheArinav@users.noreply.github.com>
2024-07-07 04:14:26 +0300

    Infrastructure build 4 - Implemented ChatRoomHost.h && ChatRoomHost.cpp - Adjusted RegisteredClient.h

--- src/Classes/Backend/ChatRoomHost.h
+++ src/Classes/Backend/ChatRoomHost.h
@@ -1,16 +1,152 @@
-//
-// Created by ariel on 7/5/2024.
-//
+#ifndef CHATROOMHOST_H
+#define CHATROOMHOST_H
 
-#ifndef CHATROOMSERVER_CHATROOMHOST_H
-#define CHATROOMSERVER_CHATROOMHOST_H
+#include <vector>
+#include "../GeneralTypes/Message.h"
+#include "RegisteredClient.h"
+
+using std::vector;
+using GeneralTypes::Message;
 
 namespace Backend {
-
     class ChatRoomHost {
+    public:
+        //region Properties
+        /**
+         * @brief The logical ID of the ChatRoomHost instance.
+         */
+        unsigned long ID;
 
+        /**
+         * @brief The logical ID of the primary owner of the ChatRoomHost instance.
+         */
+        unsigned long CreatorID;
+
+        /**
+         * @brief The secondary owners of the ChatRoomHost instance.
+         */
+        vector<RegisteredClient*> Managers;
+
+        /**
+         * @brief The client members of the ChatRoomHost instance. (Includes all ranks)
+         */
+        vector<RegisteredClient*> Members;
+
+        /**
+         * All messages visible in the ChatRoomHost instance.
+         */
+        vector<Message*> MessageList;
+        //endregion
+
+        //region Constructors
+        /**
+         * Create a new ChatRoomHost instance.
+         * @param creatorID Logical id of the creator client.
+         */
+        ChatRoomHost(unsigned long creatorID);
+        //endregion
+
+        //region 'Add' Methods
+        /**
+        * Add a new latest message to the chatroom.
+        * @param msg Latest message to add.
+        * @return Whether the message was successfully added.
+        */
+        bool PushMessage(Message* msg);
+
+        /**
+         * Add a new member to the chatroom.
+         * @param client Client to add.
+         * @return Whether the client was successfully added.
+         */
+        bool PushMember(RegisteredClient *client);
+
+        /**
+         * Add an EXISTING member to the manager list.
+         * @param client Client member to promote to manager.
+         * @return Whether the client was successfully promoted.
+         */
+        bool PromoteMember(RegisteredClient *client);
+        //endregion
+
+        //region 'Remove' Methods
+        /**
+         * Delete a message from the chatroom.
+         * @param msg Message to delete.
+         * @return Whether the message was successfully deleted.
+         */
+        bool RemoveMessage(Message *msg);
+
+        /**
+         * Remove a member from the chatroom.
+         * @param client Client member to remove.
+         * @return Whether the member was successfully removed or not.
+         */
+        bool RemoveMember(RegisteredClient *client);
+
+        /**
+         * Remove manager permissions from a member.
+         * @param client Client member to demote.
+         * @return Whether the member was successfully demoted.
+         */
+        bool DemoteMember(RegisteredClient *client);
+        //endregion
+
+    private:
+        /**
+         * Number of ChatRoomHost instances created. Used for assigning logical IDs.
+         */
+        static int count;
+
+        //region 'Search' Methods
+        /**
+         * Check if a message is registered in the chatroom.
+         * @param msg Message to search.
+         * @return Whether the message was found.
+         */
+        bool SearchMessage(Message *msg);
+
+        /**
+         * Check if a client is registered as a member of this chatroom.
+         * @param id Client member ID to search.
+         * @return Whether the client was found.
+         */
+        bool SearchMember(unsigned long id);
+
+        /**
+         * Check if a client is registered as a manager member of this chatroom.
+         * @param id Client ID to search.
+         * @return Whether the client was found in the managers list.
+         */
+        bool SearchManager(unsigned long id);
+        //endregion
+
+        //region 'Find' Methods
+        /**
+         * Given that a message exists, find the index where its pointer is stored in the vector.
+         * @param id Message ID to find.
+         * @return The message index.
+         * @warning Returns -1 if the message was not found.
+         */
+        int FindMessage(unsigned long id);
+
+        /**
+         * Given that a member exists, find the index where its pointer is stored in the vector.
+         * @param id Member client ID to find.
+         * @return The member index.
+         * @warning Returns -1 if the member was not found.
+         */
+        int FindMember(unsigned long id);
+
+        /**
+         * Given that a manager member exists, find the index where its pointer is stored in the vector.
+         * @param id Manager member client ID to find.
+         * @return The manager member index.
+         * @warning Returns -1 if the manager member was not found.
+         */
+        int FindManager(unsigned long id);
+        //endregion
     };
+} // namespace Backend
 
-} // Backend
-
-#endif //CHATROOMSERVER_CHATROOMHOST_H
+#endif // CHATROOMHOST_H
--- src/Classes/Backend/RegisteredClient.h
+++ src/Classes/Backend/RegisteredClient.h
@@ -1,64 +1,107 @@
-//
-// Created by ariel on 7/5/2024.
-//
-
-#ifndef CHATROOMSERVER_REGISTEREDCLIENT_H
-#define CHATROOMSERVER_REGISTEREDCLIENT_H
+#ifndef REGISTEREDCLIENT_H
+#define REGISTEREDCLIENT_H
 
 #include <string>
 #include <stack>
+#include <netinet/in.h>
 #include <vector>
-#include "netinet/in.h"
+#include <chrono>
 #include "../GeneralTypes/Message.h"
+#include "../GeneralTypes/MessageQueue.h"
+
+using std::string;
+using std::vector;
+using std::move;
+using GeneralTypes::Message;
+using GeneralTypes::MessageQueue;
 
 namespace Backend {
-    using std::string;
-    using std::stack;
-    using std::vector;
-    using std::move;
-    using GeneralTypes::Message;
-    using GeneralTypes::Message;
+    class RegisteredClient {
+    public:
+        //region Properties
+        /**
+         * Logical identifier of client.
+         */
+        unsigned long ID;
+        /**
+         * @brief Client name to be displayed to users.
+         */
+        string DisplayName;
+        /**
+         * Incoming messages buffer for this client.
+         */
+        MessageQueue IncomingMessages;
+        /**
+         * Socket address.
+         */
+        sockaddr_in Address;
+        /**
+         * Is client currently connected.
+         */
+        bool IsConnected;
+        /**
+         * Time of latest activity by client.
+         */
+        time_t LastActivity;
+        /**
+         * List of friends.
+         */
+        vector<RegisteredClient*> Friends;
+        //endregion
 
-    namespace Backend {
-        class RegisteredClient {
-        public:
-            //region Properties
-            /**
-             * Logical identifier of client.
-             */
-            unsigned long ID;
-            /**
-             * Client name to be displayed to users.
-             */
-            string DisplayName;
-            /**
-             * Incoming messages buffer for this client.
-             */
-            MessageQueue IncomingMessages;
-            /**
-             * Socket address.
-             */
-            sockaddr_in Address;
-            /**
-             * Is client currently connected.
-             */
-            bool IsConnected;
-            /**
-             * Time of latest activity by client.
-             */
-            time_t LastActivity;
-            /**
-             * List of freinds.
-             */
-            vector<RegisteredClient*> Friends;
-            //endregion
+        //region Constructors/Destructors
+        /*!
+         * Create new RegisteredClient instance
+         * @param DisplayName The display name of the client.
+         * @param Address     Address of the client.
+         */
+        RegisteredClient(string DisplayName, sockaddr_in Address);
 
-            //region Constructors/Destructors
-            RegisteredClient(long, string, sockaddr_in);
-            //endregion
-        };
-    } // Backend
+        // Default destructor
+        ~RegisteredClient() = default;
+        //endregion
 
-} // Backend
+        //region Methods
+        /**
+         * @brief Add a new friend of the client if they are not already friends.
+         * @param Client Pointer to the client to add as a friend to self.
+         * @return whether a change had occurred.
+         */
+        bool AddFriend(RegisteredClient *Client);
 
-#endif //CHATROOMSERVER_REGISTEREDCLIENT_H
+        /**
+         * @brief Checks whether client has a friend with the given ID.
+         * @param ID ID of a client to check
+         * @return whether client has a friend with the given ID.
+         */
+        bool HasFriend(long ID);
+
+        /**
+         * Pushes a Message to the client's incoming message buffer (if it doesn't already exist there).
+         * @param msg Message to push
+         * @return Whether the message was pushed or not.
+         */
+        bool PushMessage(Message *msg);
+
+        /**
+         * Retrieves message at the top of the stack.
+         * @return Message object from the top of the stack.
+         */
+        Message PopMessage();
+
+        /**
+         * Update the LastActivity time to the current.
+         */
+        void UpdateTime();
+        //endregion
+
+    private:
+        /*!
+         * Stores number of created users. Used to assign IDs.
+         */
+        static int count;
+    };
+
+} // namespace Backend
+
+#endif // REGISTEREDCLIENT_H
