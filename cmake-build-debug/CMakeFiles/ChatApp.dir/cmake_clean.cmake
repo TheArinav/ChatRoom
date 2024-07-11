7fbf74f8faea0ed042b64be845c9b90e1b1f54e2
TheArinav <88740085+TheArinav@users.noreply.github.com>
2024-07-11 14:35:56 +0300

    Frontend Pre1.0.1 - Fixed minor issue with logo

---
+++ cmake-build-debug/CMakeFiles/ChatApp.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/BackendComms.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/BackendComms.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/ChatRoomHost.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/ChatRoomHost.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/RegisteredClient.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/RegisteredClient.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/Server.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/Backend/Server.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/Frontend/Account.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/Frontend/Account.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/ClientAction.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/ClientAction.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/Message.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/Message.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/MessageQueue.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/MessageQueue.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/ServerAction.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/Classes/GeneralTypes/ServerAction.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/IO/InstructionInterpreter.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/IO/InstructionInterpreter.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/IO/Terminal.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/IO/Terminal.cpp.o.d"
+  "CMakeFiles/ChatApp.dir/src/main.cpp.o"
+  "CMakeFiles/ChatApp.dir/src/main.cpp.o.d"
+  "ChatApp"
+  "ChatApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
